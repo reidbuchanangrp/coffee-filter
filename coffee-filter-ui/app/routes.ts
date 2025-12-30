5fc3a60dc8444158a66927b7f99bed5d505141db
reidbuchanangrp <142035946+reidbuchanangrp@users.noreply.github.com>
2025-12-30 12:58:46 -0600

    fix missing route

--- coffee-filter-ui/app/routes.ts
+++ coffee-filter-ui/app/routes.ts
@@ -1,11 +1,7 @@
-import {
-  type RouteConfig,
-  index,
-  route,
-  layout,
-} from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
+  route("shop/:slug", "routes/home.tsx"),
   route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
