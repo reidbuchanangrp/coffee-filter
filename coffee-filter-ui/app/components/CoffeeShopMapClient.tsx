aa9ce35bae87b99cc52d434bdb8f83e06f58574b
reidbuchanangrp <142035946+reidbuchanangrp@users.noreply.github.com>
2026-04-28 08:43:22 -0500

    another test

--- coffee-filter-ui/app/components/CoffeeShopMapClient.tsx
+++ coffee-filter-ui/app/components/CoffeeShopMapClient.tsx
@@ -287,8 +287,8 @@ export function CoffeeShopMapClient({
       )}
       <TileLayer
         noWrap
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <MarkerClusterGroup
         key={coffeeShops.map((s) => s.id).join(",")}
